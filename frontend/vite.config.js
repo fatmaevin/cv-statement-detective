f0be11c75e828949de2a3ec7da55ba1924a65ff4
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2026-03-09 20:33:24 +0000

    fix: adjust Vite config to fix 404 errors on deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,17 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   build: {
     outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: "frontend/index.html",
+        createGame: "frontend/pages/create-game.html",
+        join: "frontend/pages/join-game.html",
+        waitingRoom: "frontend/pages/waiting-room.html",
+        waitingResult: "frontend/pages/waiting-result.html",
+        game: "frontend/pages/game.html",
+        result: "frontend/pages/result.html",
+      },
+    },
   },
   plugins: [tailwindcss()],
 });
