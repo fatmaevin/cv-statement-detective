0c4bb56978ab9b849ddbfe4692a6cefcf872a480
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2026-03-19 21:50:39 +0000

    Add foreign keys and model relationships

--- backend/app/models/player.py
+++ backend/app/models/player.py
@@ -1,6 +1,4 @@
-import uuid
 from sqlalchemy import Column,Integer, String, ForeignKey
-from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from app.database import Base
 
@@ -8,5 +6,7 @@ class Player(Base):
     __tablename__ = "players"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    game_id = Column(Integer, nullable=True)
+    game_id = Column(Integer,ForeignKey("games.id"), nullable=False)
     name = Column(String, nullable=False)
+
+    game = relationship("Game", backref="players")
