f1528aefa3ffac4fd884dc9535a5a1b2e8f7435d
delmorallopez <124817272+delmorallopez@users.noreply.github.com>
2026-03-23 12:55:58 +0000

    allow origin all ["*"]

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,10 +16,7 @@ app = FastAPI()
 # CORS configuration to allow requests from the frontend (running on localhost:5173)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
