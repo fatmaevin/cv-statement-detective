5a85fafd7641f3c450c431b1400033df6b85431f
sheida-shab <shabankary@gmail.com>
2026-03-24 10:18:00 +0000

    load and render current statement

---
+++ frontend/js/game.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const gameId = urlParams.get("game_id");
+  console.log("game id=", gameId);
+
+  const submitButton = document.getElementById("submit-guess-btn");
+  //-----------Load Statement---------
+  async function loadStatement() {
+    const response = await fetch(
+      `http://localhost:8000/games/${gameId}/current-statement`,
+    );
+
+    const data = await response.json();
+    console.log("statement:", data);
+
+    //handle backend empty state
+    if (data.detail) {
+      console.log("No more statements:", data.detail);
+      return null;
+    }
+
+    submitButton.disabled = true;
+    renderStatement(data);
+    return data;
+  }
+
+  function renderStatement(data) {
+    const statementText = document.getElementById("statement-text");
+
+    if (!data || !data.statement) {
+      statementText.textContent = "NO Statement available!";
+      return;
+    }
+
+    statementText.textContent = data.statement;
+    window.currentStatementId = data.statement_id;
+  }
+
+  loadStatement();
+});
