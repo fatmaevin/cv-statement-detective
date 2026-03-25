1bb310c72ac9509fa0b364f3ee333a24e5d586ed
sheida-shab <shabankary@gmail.com>
2026-03-25 23:38:53 +0000

    Add current_statement_id to games to use for player state synchronization

--- backend/app/models/game.py
+++ backend/app/models/game.py
@@ -14,3 +14,4 @@ class Game(Base):
     status = Column(String, default="waiting")
     created_at = Column(String, default=lambda: datetime.utcnow().isoformat(), nullable=False)
     ended_at = Column(String, nullable=True)
+    current_statement_id=Column(Integer,nullable=True)
