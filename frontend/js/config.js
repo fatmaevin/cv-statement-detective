856764dbf8b326487656bd687e0fcac478a08e3e
sheida-shab <shabankary@gmail.com>
2026-04-08 23:10:24 +0100

    Change statement duration to 2 minutes

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -6,7 +6,7 @@ const config = {
     production: "https://api.hosting.codeyourfuture.io",
   },
   timer: {
-    statementTimer: 25000,
+    statementTimer: 120000,//2 minutes
     gamePollingInterval: 3000,
   },
   validation: {
